117070eb351966085bd4b210f2239c0f5c83dcfb
Yogagreazy <994965979@qq.com>
2019-09-19 19:17:00 +0800

    Golang入门语法训练

---
+++ src/test/001_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"fmt"
+	"time"
+	"testing"
+)
+
+/**
+官网指南GO 第一节
+ */
+func TestStructFunc(t *testing.T) {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is ",time.Now())
+}
